e6874096eb5e65be243f107a389009fa986e19a7
myin <minyin142p@gmail.com>
2024-12-09 21:03:18 +0100

    add base path

--- apps/randomizer/vite.config.ts
+++ apps/randomizer/vite.config.ts
@@ -6,6 +6,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/randomizer',
+  base: '/randomizer/',
 
   server: {
     port: 4200,
--- apps/weekly-prompt/vite.config.ts
+++ apps/weekly-prompt/vite.config.ts
@@ -6,6 +6,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/weekly-prompt',
+  base: '/weekly-prompt/',
 
   server: {
     port: 4200,
